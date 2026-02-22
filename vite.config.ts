3a4fce4cb3ba618e45eb736e482ed307a3cc525e
yufei <xjtuzhaozhao@gmail.com>
2026-02-22 13:21:42 +0000

    add a domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'/orbital-pursuit2/',// 必须和你的仓库名 orbital-pursuit 一致
+  // base:'/orbital-pursuit2/',// 必须和你的仓库名 orbital-pursuit 一致
+  base:'/',
   })
